bc2bd92680e68002484181e06a00c402591a68f5
Thiago Vicente <thiago.vicente@tivit.com>
2020-02-10 18:56:19 -0300

    decorator sync data

--- transaction/decorators.py
+++ transaction/decorators.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-from .settings import DATABASE, PATH_DATA_FILE
+from .settings import DATA_LIST, PATH_DATA_FILE, DATABASE_PATH, DATABASE_NAME
 import json
+import shelve
 
 def validate_decorator(*args, **kwargs):
     violatios = kwargs.get('violatios')
@@ -21,6 +22,28 @@ def load_data_from_file(func):
         with open(PATH_DATA_FILE) as file_json: 
             transactions = json.load(file_json)
         for index, data in  enumerate(transactions, start=1):
-            DATABASE.append(data['transaction'])
+            DATA_LIST.append(data['transaction'])
         return func()
+    return wrapper
+
+
+def sync_data(func):
+    def wrapper(*args, **kwargs):
+        database =  shelve.open(DATABASE_PATH)
+        if 'data' not in database:
+            with open(PATH_DATA_FILE) as file_json: 
+                transactions = json.load(file_json)
+            for index, data in  enumerate(transactions, start=1):
+                DATA_LIST.append(data['transaction'])
+            database['data'] = DATA_LIST
+            func() 
+        else: 
+            transactions = database['data']
+            for index, data in  enumerate(transactions, start=1):
+                DATA_LIST.append(data)
+            func()
+            if DATA_LIST != transactions:
+                database['data'] = DATA_LIST 
+
+        database.close()
     return wrapper
